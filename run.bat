63460f9088f096e296e29659637dc2a3dc47765b
Jedrick Chua <chua.jedrick@yahoo.com>
2014-10-26 12:56:36 +0800

    Update

--- run.bat
+++ run.bat
@@ -1,4 +1,6 @@
 java jflex.Main conyo.flex
 java -jar java-cup-11b.jar -interface -parser Parser conyo.cup
 javac -cp java-cup-11b-runtime.jar;. *.java
-java -cp java-cup-11b-runtime.jar;. MainConyo test.txt > output.txt
+java -cp java-cup-11b-runtime.jar;. MainConyo test1.txt > output1.txt
+java -cp java-cup-11b-runtime.jar;. MainConyo test2.txt > output2.txt
+java -cp java-cup-11b-runtime.jar;. MainConyo test3.txt > output3.txt
